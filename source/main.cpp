0f2f2d69fb2111836eb4f4b0f32721869db59f14
ndunnett <nathandunnett@me.com>
2022-08-30 10:36:01 +1000

    Drawing a triangle > graphics pipeline > render ps

--- source/main.cpp
+++ source/main.cpp
@@ -83,6 +83,7 @@ private:
         create_logical_device();
         create_swap_chain();
         create_image_views();
+        create_render_pass();
         create_graphics_pipeline();
     }
 
@@ -94,6 +95,7 @@ private:
 
     void cleanup() {
         vkDestroyPipelineLayout(device, pipeline_layout, nullptr);
+        vkDestroyRenderPass(device, render_pass, nullptr);
 
         for (auto* image_view : swap_chain_image_views) {
             vkDestroyImageView(device, image_view, nullptr);
@@ -338,6 +340,38 @@ private:
             if (vkCreateImageView(device, &ci, nullptr, &swap_chain_image_views[i]) != VK_SUCCESS) {
                 throw std::runtime_error("failed to create image views!");
             }
+        }
+    }
+
+    void create_render_pass() {
+        VkAttachmentDescription color_attachment{};
+        color_attachment.format = swap_chain_image_format;
+        color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+        color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+        color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+        color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        color_attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        color_attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+        VkAttachmentReference color_attachment_ref{};
+        color_attachment_ref.attachment = 0;
+        color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+        VkSubpassDescription subpass{};
+        subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+        subpass.colorAttachmentCount = 1;
+        subpass.pColorAttachments = &color_attachment_ref;
+
+        VkRenderPassCreateInfo render_pass_ci{};
+        render_pass_ci.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+        render_pass_ci.attachmentCount = 1;
+        render_pass_ci.pAttachments = &color_attachment;
+        render_pass_ci.subpassCount = 1;
+        render_pass_ci.pSubpasses = &subpass;
+
+        if (vkCreateRenderPass(device, &render_pass_ci, nullptr, &render_pass) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create render pass!");
         }
     }
 
@@ -723,6 +757,7 @@ private:
     VkFormat swap_chain_image_format{};
     VkExtent2D swap_chain_extent{};
     std::vector<VkImageView> swap_chain_image_views;
+    VkRenderPass render_pass{};
     VkPipelineLayout pipeline_layout{};
 };
 
